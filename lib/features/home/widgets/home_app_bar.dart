cf3356f1fb77849b73ab1cacb32eefddd5f8b90f
GASTDASH <gastdash@gmail.com>
2024-11-05 20:44:52 +0300

    Исправлена ошибка наложения аппбара

--- lib/features/home/widgets/home_app_bar.dart
+++ lib/features/home/widgets/home_app_bar.dart
@@ -11,64 +11,67 @@ class HomeAppBar extends StatelessWidget implements PreferredSize {
 
     return PreferredSize(
       preferredSize: preferredSize,
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          children: [
-            InkWell(
-              borderRadius: BorderRadius.circular(12),
-              onTap: () {},
-              child: Ink(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                    color: theme.primaryColor.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(12)),
-                child: Icon(
-                  Icons.place_outlined,
-                  color: theme.primaryColor,
-                  size: 32,
+      child: Ink(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              InkWell(
+                borderRadius: BorderRadius.circular(12),
+                onTap: () {},
+                child: Ink(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: theme.primaryColor.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Icon(
+                    Icons.place_outlined,
+                    color: theme.primaryColor,
+                    size: 32,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 16),
-            SizedBox(
-              width: MediaQuery.of(context).size.width - 170,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Ваше местоположение"),
-                  SizedBox(
-                    child: Text(
-                      "Орехово-Зуево, Московская обл.",
-                      style: theme.textTheme.titleMedium
-                          ?.copyWith(fontWeight: FontWeight.w800),
-                      overflow: TextOverflow.ellipsis,
+              const SizedBox(width: 16),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 170,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Ваше местоположение"),
+                    SizedBox(
+                      child: Text(
+                        "Орехово-Зуево, Московская обл.",
+                        style: theme.textTheme.titleMedium
+                            ?.copyWith(fontWeight: FontWeight.w800),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Expanded(child: SizedBox.expand()),
+              InkWell(
+                borderRadius: BorderRadius.circular(12),
+                onTap: () {
+                  Navigator.pushNamed(context, "/profile");
+                },
+                child: Ink(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    image: const DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage("https://i.imgur.com/867trdy.jpeg"),
                     ),
                   ),
-                ],
-              ),
-            ),
-            const Expanded(child: SizedBox.expand()),
-            InkWell(
-              borderRadius: BorderRadius.circular(12),
-              onTap: () {
-                Navigator.pushNamed(context, "/profile");
-              },
-              child: Ink(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  image: const DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage("https://i.imgur.com/867trdy.jpeg"),
-                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
