d2addcc15a43587e0e9d50eb23a177043a09baf7
GASTDASH <gastdash@gmail.com>
2025-01-12 22:21:20 +0300

    Добавление анимации в блоке отслеживания по коду

--- lib/features/home/widgets/track_package_card.dart
+++ lib/features/home/widgets/track_package_card.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class TrackPackageCard extends StatelessWidget {
+class TrackPackageCard extends StatefulWidget {
   const TrackPackageCard({
     super.key,
     required this.trackContainerKey,
   });
 
-  final GlobalKey<State<StatefulWidget>> trackContainerKey;
+  final GlobalKey trackContainerKey;
+
+  @override
+  State<TrackPackageCard> createState() => _TrackPackageCardState();
+}
+
+class _TrackPackageCardState extends State<TrackPackageCard> {
+  bool codeTyped = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +23,7 @@ class TrackPackageCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(24),
       child: SizedBox(
-        key: trackContainerKey,
+        key: widget.trackContainerKey,
         height: 160,
         child: Container(
           decoration: BoxDecoration(
@@ -49,10 +56,14 @@ class TrackPackageCard extends StatelessWidget {
                         ),
                         child: Center(
                           child: TextField(
+                            onChanged: (value) => setState(() =>
+                                value.isNotEmpty
+                                    ? codeTyped = true
+                                    : codeTyped = false),
+                            onTapOutside: (event) =>
+                                FocusManager.instance.primaryFocus?.unfocus(),
                             decoration: InputDecoration(
-                              prefixIcon:
-                                  // const Icon(Icons.mail_outlined),
-                                  Padding(
+                              prefixIcon: Padding(
                                 padding: const EdgeInsets.all(8.0),
                                 child: SvgPicture.asset(
                                   "assets/icons/box.svg",
@@ -83,10 +94,32 @@ class TrackPackageCard extends StatelessWidget {
                           color: theme.primaryColor,
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        child: const Icon(
-                          Icons.qr_code,
-                          color: Colors.white,
-                          size: 34,
+                        child: AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 300),
+                          switchInCurve: Curves.easeOutExpo,
+                          transitionBuilder: (child, animation) =>
+                              RotationTransition(
+                            turns: child.key == const ValueKey('icon1')
+                                ? Tween<double>(begin: 1.25, end: 1)
+                                    .animate(animation)
+                                : Tween<double>(begin: 0.75, end: 1)
+                                    .animate(animation),
+                            child:
+                                ScaleTransition(scale: animation, child: child),
+                          ),
+                          child: codeTyped
+                              ? const Icon(
+                                  key: ValueKey('icon2'),
+                                  Icons.send,
+                                  color: Colors.white,
+                                  size: 34,
+                                )
+                              : const Icon(
+                                  key: ValueKey('icon1'),
+                                  Icons.qr_code,
+                                  color: Colors.white,
+                                  size: 34,
+                                ),
                         ),
                       ),
                     ),
