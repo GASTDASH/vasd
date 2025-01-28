986ab33a730869837f47cfe46d43d9ba72e3b4a7
GASTDASH <gastdash@gmail.com>
2025-01-28 21:33:19 +0300

    Добавлен Hive

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
@@ -18,6 +19,8 @@ Future<void> main() async {
       url: "https://qprxzwerdumhcourygam.supabase.co",
       anonKey:
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwcnh6d2VyZHVtaGNvdXJ5Z2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4Nzc4OTksImV4cCI6MjA0NjQ1Mzg5OX0.2hmdN4gUgyX9JOKuOiL6Cf0LNdOHAn2OfKRhmtrJC10");
+
+  await Hive.initFlutter();
 
   final dio = Dio();
   final talker = Talker();
